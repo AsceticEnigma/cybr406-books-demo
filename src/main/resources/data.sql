6b800b004ef583efad36a582536addd2abfa5783
Ry Lowry <rylowry@gmail.com>
2020-02-10 22:19:40 -0600

    updated data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-insert into author (username, name, bio) values ('frankh', 'Frank Herbert', 'Franklin Patrick Herbert, Jr. (October 8, 1920 – February 11, 1986) was an American science fiction writer best known for the novel Dune and its five sequels.')
+insert into author (username, name, bio) values ('frankh', 'Frank Herbert', 'Franklin Patrick Herbert, Jr. (October 8, 1920 - February 11, 1986) was an American science fiction writer best known for the novel Dune and its five sequels.')
 insert into book (author_id, title) values (1, 'Dune')
 insert into book (author_id, title) values (1, 'Dune Messiah')
 insert into book (author_id, title) values (1, 'Children of Dune')
 
-insert into author (username, name, bio) values ('philipkd', 'Philip K. Dick', 'Philip Kindred Dick (December 16, 1928 – March 2, 1982) was an American writer known for his work in science fiction.')
+insert into author (username, name, bio) values ('philipkd', 'Philip K. Dick', 'Philip Kindred Dick (December 16, 1928 - March 2, 1982) was an American writer known for his work in science fiction.')
 insert into book (author_id, title) values (2, 'Do Androids Dream of Electric Sheep?')
 insert into book (author_id, title) values (2, 'The Man in the High Castle')
 insert into book (author_id, title) values (2, 'A Scanner Darkly')
