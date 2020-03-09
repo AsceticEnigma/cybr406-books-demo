226a9a9acde77f9592f6a7a0f4108f14bf9ab35a
Ry Lowry <rylowry@gmail.com>
2020-03-09 17:09:04 -0500

    Security Demo 03 - InMemoryUserDetailsManager

--- src/main/java/com/cybr406/bookdemo/SecurityConfiguration.java
+++ src/main/java/com/cybr406/bookdemo/SecurityConfiguration.java
@@ -1,13 +1,32 @@
 package com.cybr406.bookdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        User.UserBuilder builder = User.withDefaultPasswordEncoder();
+
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        userDetailsManager.createUser(builder
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build());
+
+        return userDetailsManager;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
