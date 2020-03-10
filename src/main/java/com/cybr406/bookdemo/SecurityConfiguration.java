dd17f6d7bd59048b92a9f3ff9b6428d5c3da3c9e
Ry Lowry <rylowry@gmail.com>
2020-03-09 23:15:10 -0500

    Security Demo 04 - Basic JDBC Setup

--- src/main/java/com/cybr406/bookdemo/SecurityConfiguration.java
+++ src/main/java/com/cybr406/bookdemo/SecurityConfiguration.java
@@ -1,35 +1,90 @@
 package com.cybr406.bookdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Value("${spring.h2.console.enabled}")
+    boolean h2ConsoleEnabled;
+
+    @Autowired
+    DataSource dataSource;
+
     @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        User.UserBuilder builder = User.withDefaultPasswordEncoder();
+    JdbcUserDetailsManager jdbcUserDetailsManager() {
+        // We plan to store and retrieve user details from a database.
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
-        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-        userDetailsManager.createUser(builder
-                .username("admin")
-                .password("admin")
-                .roles("ADMIN")
-                .build());
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        // A DelegatingPasswordEncoder can handle numerous different password encodings based on the prefix
+        // of the password.
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
-        return userDetailsManager;
+    @Bean
+    User.UserBuilder userBuilder() {
+        // Having a User.UserBuilder instance as an @Bean that can be @Autowired can be helpful.
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        User.UserBuilder users = User.builder();
+        users.passwordEncoder(passwordEncoder::encode);
+        return users;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // Configure authentication to use the database.
+        // Provide hardcoded users (never put passwords in source code in anything other than a demo)
+        User.UserBuilder users = userBuilder();
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(users
+                        .username("admin")
+                        .password("admin")
+                        .roles("ADMIN"))
+                .withUser(users
+                        .username("frankh")
+                        .password("password")
+                        .roles("AUTHOR"))
+                .withUser(users
+                        .username("philipkd")
+                        .password("password")
+                        .roles("AUTHOR"));
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // Configure security for the h2 console. This should only ever happen in dev environments.
+        if (h2ConsoleEnabled) {
+            http
+                    .authorizeRequests()
+                    .antMatchers("/h2-console", "/h2-console/**").permitAll();
+
+            // By default, frame options is set to DENY. The h2 console is rendered in a frame, however. Changing to
+            // SAMEORIGIN allows the content to appear since it is originating from the same server. DENY is a better
+            // option for prod, where the h2 console should be disabled anyhow.
+            // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+            http.headers().frameOptions().sameOrigin();
+        }
+
         // Customize access here using the http object
         http.authorizeRequests()
                 .mvcMatchers(HttpMethod.GET, "/**").permitAll()  // Allow all read-only requests using GET
